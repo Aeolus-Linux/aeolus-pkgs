f9e75450adbdfbfbe55179eda6033da1ce594d0c
Eile <tysayahi@gmail.com>
2020-10-12 14:34:05 -0400

    try to get pacman to install stuff
    Former-commit-id: 37e06b21fe679346fc0594845819fdff66f0fa03

--- account.sh
+++ account.sh
@@ -2,6 +2,7 @@ DIR="$(pwd)"
 echo test1
 pacman -Syu archiso sudo fakeroot git base-devel jq giflib libid3tag libqalculate rofi libwebp  imlib2 --noconfirm --noprogressbar 
 pacman -Syu startup-notification expac diffstat pacutils wget devtools sudo libxslt cmake intltool mesa gtk-doc gtk-update-icon-cache meson libx11 libxkbcommon gobject-introspection gnome-common polkit gtk3 dbus-glib go pkg-config libxcb xcb-util pam cairo check libglvnd libev ncurses xorg xorg-xauth glibc base-devel gcc libx11 libxkbcommon>=0.5.0 libxkbcommon-x11>=0.5.0 --noconfirm --noprogressbar 
+pacman -Sy sudo --noconfirm --noprogressbar
 echo test2
 tee -a /etc/sudoers > /dev/null <<EOT
 nobody    ALL=(ALL) NOPASSWD:ALL
--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+sudo pacman -Syu archiso sudo fakeroot git base-devel jq giflib libid3tag libqalculate rofi libwebp  imlib2 --noconfirm --noprogressbar 
+sudo pacman -Syu startup-notification expac diffstat pacutils wget devtools sudo libxslt cmake intltool mesa gtk-doc gtk-update-icon-cache meson libx11 libxkbcommon gobject-introspection gnome-common polkit gtk3 dbus-glib go pkg-config libxcb xcb-util pam cairo check libglvnd libev ncurses xorg xorg-xauth glibc base-devel gcc libx11 libxkbcommon>=0.5.0 libxkbcommon-x11>=0.5.0 --noconfirm --noprogressbar 
 sudo mkdir -p /go/pkg/mod/cache/download/github.com
 sudo chmod -R 777 /go
 sudo chmod -R 777 /go/pkg/mod/cache/download/github.com
