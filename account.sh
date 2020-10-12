aa1a3e3e5ff5ec08a96def9b6026b4f88f3cfcc9
Eile Kerning <tysayahi@gmail.com>
2020-10-12 13:01:09 -0400

    Update account.sh
    Former-commit-id: dd1b606e70f1361068a31dbb5f84405438e07111

--- account.sh
+++ account.sh
@@ -1,4 +1,4 @@
-pacman -Syu --needed archiso git base-devel jq expac diffstat pacutils wget devtools sudo libxslt cmake intltool mesa gtk-doc gtk-update-icon-cache meson libx11 libxkbcommon gobject-introspection gnome-common polkit gtk3 dbus-glib go pkg-config libxcb xcb-util pam cairo check libglvnd libev ncurses xorg xorg-xauth glibc base-devel gcc libx11 libxkbcommon>=0.5.0 libxkbcommon-x11>=0.5.0 --noconfirm --noprogressbar # Install packages we'll need to build
+pacman -Syu --needed archiso git base-devel jq giflib-5.2.1-2  libid3tag-0.15.1b-11  libwebp-1.1.0-1  imlib2-1.7.0-1  startup-notification-0.12-7 expac diffstat pacutils wget devtools sudo libxslt cmake intltool mesa gtk-doc gtk-update-icon-cache meson libx11 libxkbcommon gobject-introspection gnome-common polkit gtk3 dbus-glib go pkg-config libxcb xcb-util pam cairo check libglvnd libev ncurses xorg xorg-xauth glibc base-devel gcc libx11 libxkbcommon>=0.5.0 libxkbcommon-x11>=0.5.0 --noconfirm --noprogressbar # Install packages we'll need to build
 tee -a /etc/sudoers > /dev/null <<EOT
 nobody    ALL=(ALL) NOPASSWD:ALL
 EOT
