83f3cf6075ba5a2ecc37d121b7a4fbec6ea9d5dd
Eile Kerning <tysayahi@gmail.com>
2020-10-12 12:55:11 -0400

    Update main.sh
    Former-commit-id: 6cb07c2e462486a6945572e63d884926133702c0

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
-        ./setup.sh
+sudo mkdir -p /go/pkg/mod/cache/download/github.com
+sudo chmod -R 777 /go
+sudo chmod -R 777 /go/pkg/mod/cache/download/github.com
+./setup.sh
         ./pkg_aur.sh
        sh ./x86_64/update.sh
