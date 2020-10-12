7c2f413ff3e3d68ad6283c9e5784040ad33563ef
Eile <tysayahi@gmail.com>
2020-10-12 14:08:19 -0400

    Update account.sh
    Former-commit-id: ce1e8343cfc08b98f27cfebcbcc6b62267eddef0

--- account.sh
+++ account.sh
@@ -1,6 +1,6 @@
 DIR="$(pwd)"
 DEPS="$(cat ${DIR}/deps.list)"
-pacman -Syu --needed ${DEPS} --noconfirm --noprogressbar # Install packages we'll need to build
+pacman -Syu --needed "$(cat ${DIR{/deps.list)" --noconfirm --noprogressbar # Install packages we'll need to build
 tee -a /etc/sudoers > /dev/null <<EOT
 nobody    ALL=(ALL) NOPASSWD:ALL
 EOT
