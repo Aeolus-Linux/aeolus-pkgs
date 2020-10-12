a3a460fab0a9a1adf199dce9791ef2e8cbd73a33
GitHub Action <action@github.com>
2020-10-12 05:32:10 +0000

    Add changes
    Former-commit-id: 83f4b1df8ad32cbb18646fef934ec6126bd03e99

---
+++ account.sh
@@ -0,0 +1,6 @@
+pacman -Syu archiso git base-devel jq expac diffstat pacutils wget devtools sudo libxslt cmake intltool mesa gtk-doc gtk-update-icon-cache meson libx11 libxkbcommon gobject-introspection gnome-common polkit gtk3 dbus-glib go pkg-config libxcb xcb-util pam cairo check libglvnd libev ncurses xorg xorg-xauth glibc base-devel gcc libx11 libxkbcommon>=0.5.0 libxkbcommon-x11>=0.5.0 --noconfirm --noprogressbar # Install packages we'll need to build
+tee -a /etc/sudoers > /dev/null <<EOT
+nobody    ALL=(ALL) NOPASSWD:ALL
+EOT
+chmod -R 777 .
+su -s /bin/sh nobody -c "./main.sh"
---
+++ main.sh
@@ -0,0 +1,4 @@
+        ./setup.sh
+        ./pkg_aur.sh
+        ./pkg_lme.sh
+        ./x86_64/update.sh
