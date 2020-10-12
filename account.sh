a9d909eeb7270fc1ffd75ed3bba8b809b5857353
Eile <tysayahi@gmail.com>
2020-10-12 15:35:29 -0400

    Update account.sh
    Former-commit-id: 93f9e466fc688350131c0f3cc55ac7b022598019

--- account.sh
+++ account.sh
@@ -1,5 +1,5 @@
 pacman -Syu --needed --noconfirm --noprogressbar 
-pacman -Sy autogen efibootmgr base-devel git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu --needed --noconfirm --noprogressbar 
+pacman -Sy autogen efibootmgr base-devel python rsync git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu --needed --noconfirm --noprogressbar 
 tee -a /etc/sudoers > /dev/null <<EOT
 nobody    ALL=(ALL) NOPASSWD:ALL
 EOT
