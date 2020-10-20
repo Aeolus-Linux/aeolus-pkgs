76d7362f6e621bee8b5ae0f987c07c26bbc583f6
Eile <tysayahi@gmail.com>
2020-10-20 09:17:29 -0400

    Update account.sh
    Former-commit-id: fb24b09d3abda87d8a114e9626bd22ce10890652

--- account.sh
+++ account.sh
@@ -1,6 +1,6 @@
 pacman -Syu --needed --noconfirm --noprogressbar 
 pacman -S --needed git base-devel
-pacman -Sy --needed autogen perl-data-dump perl-linux-desktopfiles perl gtk2-perl perl-file-desktopentry perl-gtk3 efibootmgr base-devel python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor imlib2 libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu --needed --noconfirm --noprogressbar 
+pacman -Sy --needed autogen perl-data-dump perl-linux-desktopfiles perl sudo fakeroot gtk2-perl perl-file-desktopentry perl-gtk3 efibootmgr base-devel python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor imlib2 libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu --needed --noconfirm --noprogressbar 
 tee -a /etc/sudoers > /dev/null <<EOT
 nobody    ALL=(ALL) NOPASSWD:ALL
 EOT
