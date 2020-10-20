d1371c9ee5ab6cbcdc27d4bdb965e9bef2204a0a
Eile <tysayahi@gmail.com>
2020-10-20 13:46:50 -0400

    Update account.sh
    Former-commit-id: ebe3ae15940fb5c8032f809cfd62f374cef93d2d

--- account.sh
+++ account.sh
@@ -1,16 +1,17 @@
 pacman -Syu --needed --noconfirm --noprogressbar 
 pacman -S --needed git base-devel
+adduser --disabled-password --gecos "" nobody
+echo "nobody" | passwd --stdin nobody
 pacman -Sy --needed autogen perl-data-dump perl sudo fakeroot gtk2-perl perl-file-desktopentry perl-gtk3 efibootmgr base-devel python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor imlib2 libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu --needed --noconfirm --noprogressbar 
 tee -a /etc/sudoers > /dev/null <<EOT
 nobody    ALL=(ALL) NOPASSWD:ALL
 EOT
 chmod -R 777 .
-echo "nobody" | passwd --stdin nobody
 sudo mkdir -p /go/pkg/mod/cache/download/github.com
 sudo chmod -R 777 /go
 sudo chmod -R 777 /go/pkg/mod/cache/download/github.com
 sudo mkdir -p /.cache/go-build
 sudo chmod -R 777 /.cache
 sudo chmod -R 777 /.cache/go-build
-su -s /bin/sh nobody -c "./yay.sh"
-su -s /bin/sh nobody -c "./main.sh"
+su -c "./yay.sh" nobody
+su -c "./main.sh" nobody
