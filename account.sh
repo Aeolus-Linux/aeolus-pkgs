a913d83277b7b359865a9caebe726380ad2d7432
Eile <tysayahi@gmail.com>
2020-10-20 09:13:05 -0400

    Fix yay
    Former-commit-id: 4aad0c53124408ea889c667367d108f367917483

--- account.sh
+++ account.sh
@@ -1,12 +1,9 @@
 pacman -Syu --needed --noconfirm --noprogressbar 
 pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-yay -Sy openbox-patched
 pacman -Sy --needed autogen perl-data-dump perl-linux-desktopfiles perl gtk2-perl perl-file-desktopentry perl-gtk3 efibootmgr base-devel python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor imlib2 libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu --needed --noconfirm --noprogressbar 
 tee -a /etc/sudoers > /dev/null <<EOT
 nobody    ALL=(ALL) NOPASSWD:ALL
 EOT
 chmod -R 777 .
+su -s /bin/sh nobody -c "./yay.sh"
 su -s /bin/sh nobody -c "./main.sh"
