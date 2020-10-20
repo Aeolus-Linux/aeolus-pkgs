d62bc2c36e292d28bc1a045c5ed4ed2893db1062
Eile <tysayahi@gmail.com>
2020-10-20 14:12:22 -0400

    Update account.sh
    Former-commit-id: 8775e9be4236a3d68e6917ab648e8499ca797150

--- account.sh
+++ account.sh
@@ -1,11 +1,11 @@
 pacman -Syu --needed --noconfirm --noprogressbar 
 pacman -S --needed git base-devel
-useradd -p nobody  -c nobody -m nobody
-echo "nobody:nobody"|chpasswd
+useradd -p builderbob -c builderbob -m builderbob
+echo "builderbob:builderbob"|chpasswd
 #echo "nobody" | passwd --stdin nobody
 pacman -Sy --needed autogen perl-data-dump perl sudo fakeroot gtk2-perl perl-file-desktopentry perl-gtk3 efibootmgr base-devel python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor imlib2 libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu --needed --noconfirm --noprogressbar 
 tee -a /etc/sudoers > /dev/null <<EOT
-nobody    ALL=(ALL) NOPASSWD:ALL
+builderbob    ALL=(ALL) NOPASSWD:ALL
 EOT
 chmod -R 777 .
 sudo mkdir -p /go/pkg/mod/cache/download/github.com
@@ -14,5 +14,5 @@ sudo chmod -R 777 /go/pkg/mod/cache/download/github.com
 sudo mkdir -p /.cache/go-build
 sudo chmod -R 777 /.cache
 sudo chmod -R 777 /.cache/go-build
-su -c "./yay.sh" nobody
-su -c "./main.sh" nobody
+su -c "./yay.sh" builderbob
+su -c "./main.sh" builderbob
