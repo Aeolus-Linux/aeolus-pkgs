81c73cf2123991dddb7970c3d6c4cf04ab1db531
Eile <tysayahi@gmail.com>
2020-10-20 14:07:05 -0400

    Update account.sh
    Former-commit-id: fffc53449b84a1447335e680ee75b462968bc6bb

--- account.sh
+++ account.sh
@@ -1,12 +1,12 @@
 pacman -Syu --needed --noconfirm --noprogressbar 
 pacman -S --needed git base-devel
-useradd --disabled-password --gecos "" nobody
+useradd -p nobody  -c nobody -m nobody
 echo "nobody:nobody"|chpasswd
 #echo "nobody" | passwd --stdin nobody
 pacman -Sy --needed autogen perl-data-dump perl sudo fakeroot gtk2-perl perl-file-desktopentry perl-gtk3 efibootmgr base-devel python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor imlib2 libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu --needed --noconfirm --noprogressbar 
 tee -a /etc/sudoers > /dev/null <<EOT
 nobody    ALL=(ALL) NOPASSWD:ALL
-EOT 
+EOT
 chmod -R 777 .
 sudo mkdir -p /go/pkg/mod/cache/download/github.com
 sudo chmod -R 777 /go
