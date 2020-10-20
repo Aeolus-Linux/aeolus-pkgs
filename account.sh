24b8affbc0e9b55c35cb6a1aef36e6a97abd33b9
Eile <tysayahi@gmail.com>
2020-10-20 13:54:47 -0400

    Update account.sh
    Former-commit-id: 5ac41744208fd6137a4367cd83c8232d524a04f5

--- account.sh
+++ account.sh
@@ -1,6 +1,6 @@
 pacman -Syu --needed --noconfirm --noprogressbar 
 pacman -S --needed git base-devel
-adduser --disabled-password --gecos "" nobody
+echo "nobody:nobody"|chpasswd
 echo "nobody" | passwd --stdin nobody
 pacman -Sy --needed autogen perl-data-dump perl sudo fakeroot gtk2-perl perl-file-desktopentry perl-gtk3 efibootmgr base-devel python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor imlib2 libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu --needed --noconfirm --noprogressbar 
 tee -a /etc/sudoers > /dev/null <<EOT
