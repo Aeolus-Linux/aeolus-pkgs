8f91830d1e329f0b50bce476b403450d9e9175bd
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-10-24 23:43:55 -0400

    Add plymouth
    Former-commit-id: c6010fe1237b33881fe53efbe97b49f4de99332d

--- yay.sh
+++ yay.sh
@@ -1,4 +1,4 @@
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
-yay -Sy --noconfirm openbox-patched perl-linux-desktopfiles
+yay -Sy --noconfirm plymouth openbox-patched perl-linux-desktopfiles
