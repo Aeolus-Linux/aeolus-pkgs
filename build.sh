dc21881f5a59f6d8e83c149ae7774b6c4231331f
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-10-27 16:59:46 -0400

    Add custom kernel
    Former-commit-id: c0368541c1a83a74479f16a62658a2220f7aa9aa

--- build.sh
+++ build.sh
@@ -14,6 +14,12 @@ git clone https://aur.archlinux.org/yay.git
 mkdir aeolus-pkgs
 cd aeolus-pkgs
 git clone https://github.com/Aeolus-Linux/calamares-aeolus.git
+git clone https://github.com/Frogging-Family/linux-tkg.git
+cd linux-tkg
+rm customization.cfg
+cd ..
+cd ..
+cp ./customization.cfg ./aeolus-pkgs/linux-tkg
 cd ..
 pwd
 cd yay
