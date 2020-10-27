7baece55fdfc9e1330c417fdc3ce0270fcc28a3f
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-10-27 18:50:50 -0400

    Fix Linux Kernel Build
    Former-commit-id: 4de30b570c1b257325ced786ba26f9de134d1b97

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -Sy --needed --noconfirm autogen cmake python-sphinx grub git meson perl fish tmux libglade python2-cairo python2-gobject2 python2-numpy  vim zsh git go i3-wm python2 python pacman-contrib expac fzf libpulse libmpdclient perl-data-dump perl sudo fakeroot gtk2-perl perl-file-desktopentry perl-gtk3 efibootmgr base-devel python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor imlib2 libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu
+sudo pacman -Sy --needed --noconfirm coreutils kmod linux-firmware bc git graphviz imagemagickkmod libelf pahole python-spinx_rtd_theme xmlto crda autogen cmake python-sphinx grub git meson perl fish tmux libglade python2-cairo python2-gobject2 python2-numpy  vim zsh git go i3-wm python2 python pacman-contrib expac fzf libpulse libmpdclient perl-data-dump perl sudo fakeroot gtk2-perl perl-file-desktopentry perl-gtk3 efibootmgr base-devel python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor imlib2 libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu
 sudo chmod -R 777 .
 sudo mkdir -p /go/pkg/mod/cache/download/github.com
 sudo chmod -R 777 /go
@@ -11,18 +11,9 @@ sudo chmod +x ./pkg_aur.sh
 sudo chmod +x ./pkg_lme.sh
 sudo chmod +x ./x86_64/update.sh
 git clone https://aur.archlinux.org/yay.git
-mkdir aeolus-pkgs
-cd aeolus-pkgs
-git clone https://github.com/Frogging-Family/linux-tkg.git
-cd linux-tkg
-rm customization.cfg
-cd ..
-cd ..
-cp ./customization.cfg ./aeolus-pkgs/linux-tkg
-pwd
 cd yay
 makepkg -si --noconfirm
-yay -Sy --noconfirm plymouth openbox-patched perl-linux-desktopfiles mkinitcpio-openswap ckbcomp
+yay -Sy --noconfirm plymouth openbox-patched thrash-protect perl-linux-desktopfiles mkinitcpio-openswap ckbcomp
 cd ..
 git clone https://aur.archlinux.org/pygtk.git
 cd pygtk
