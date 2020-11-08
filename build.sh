50f1f78885d17faa614af001d6b102c20ddc6947
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-11-08 17:44:52 -0500

    Update build.sh
    Former-commit-id: 7b9e9b7b99a8cd4a83f487e701925a940dec1e3f

--- build.sh
+++ build.sh
@@ -62,6 +62,7 @@ pwd
 dir
 cd ..
 cd ..
+cd $BWDIR
 mv ./aeolus-pkgs/plymouth-theme-deus_ex-git/*.pkg.tar.zst ./x86_64
 #mv ./aeolus-pkgs/calamares-makepkg/*.pkg.tar.zst ./x86_64
 mv ./x86_64/speshul/*.pkg.tar.zst ./x86_64
