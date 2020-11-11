5aeb07b02bec73a7493a1a15b41fc44cab186599
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-11-10 20:27:54 -0500

    Update pkg_lme.sh
    Former-commit-id: 12844314218b14cd0253047fcdd96e81afc7466d

--- pkg_lme.sh
+++ pkg_lme.sh
@@ -11,66 +11,66 @@ PKGS=($(ls $PKGDIR))
 
 cd $PKGDIR
 cd aeolus-about-dev
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-about-dev/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-fonts
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-fonts/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-get-started
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-get-started/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-grid-icon-theme
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-grid-icon-theme/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-gtk-themepack
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-gtk-themepack/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-hybrid-icon-theme
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-hybrid-icon-theme/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-lxdm-theme
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-lxdm-theme/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-lxdm-theme-circuit
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-lxdm-theme-circuit/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-lxdm-theme-gears
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-lxdm-theme-gears/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-pixmaps
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-pixmaps/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-tint2-configs
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-tint2-configs/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd aeolus-wallpapers
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/aeolus-wallpapers/*.pkg.tar.zst ./x86_64/
 cd $PKGDIR
 cd nvidia-all-noninteractive
-makepkg -s 
+makepkg -sf 
 cd $DIR
 mv ./aeolus-pkgs/nvidia-all-noninteractive/*.pkg.tar.zst ./x86_64/
