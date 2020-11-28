3144c9c76ca57438ee3eb9f15291dddc16593b43
Eile Kerning <tysayahi@gmail.com>
2020-11-27 19:08:24 -0500

    Update build.sh
    Former-commit-id: 8a5d8d8e20eba14060ac82d2345a6a94d789a3c0

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 BWDIR="$(pwd)"
 echo ${BWDIR}
 PKGDIRBW="$BWDIR/osmium-pkgs"
-sudo pacman -Syy --needed --noconfirm meson coreutils kmod linux-firmware bc git graphviz libinput imagemagick kmod git fakeroot libelf extra-cmake-modules pahole python-sphinx_rtd_theme xmlto crda autogen cmake 
-sudo pacman -Syy --needed --noconfirm python-sphinx grub git inkscape optipng sassc gnome-themes-extra gtk-engine-murrine meson perl fish libbsd libevdev tmux libwacom libglade python2-cairo boost python2-gobject2 qt5-tools python2-numpy  vim zsh git go i3-wm python2 python pacman-contrib expac fzf libpulse 
-sudo pacman -Syy --needed --noconfirm libmpdclient boost-libs perl-data-dump perl python-setuptools python2-setuptools sudo fakeroot mtdev gtk2-perl  tslib xorg-xset perl-file-desktopentry perl-gtk3 qt5-translations efibootmgr boost-libs python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor 
-sudo pacman -Syu --needed --noconfirm man-pages imlib2 double-conversion base-devel xmlto docbook-xsl qt5-base xdg-utils inetutils bc pahole patchutils flex python-sphinx python-sphinx_rtd_theme graphviz imagemagick inetutils patchutils libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu
+sudo pacman -Syy --needed --noconfirm meson coreutils kmod linux-firmware bc git graphviz libinput perl-socket6  imagemagick kmod git fakeroot libelf extra-cmake-modules pahole python-sphinx_rtd_theme xmlto crda autogen cmake 
+sudo pacman -Syy --needed --noconfirm python-sphinx grub git inkscape optipng perl-net-ssleay  sassc perl-encode-detect gnome-themes-extra gtk-engine-murrine meson perl fish libbsd libevdev tmux libwacom libglade python2-cairo boost python2-gobject2 qt5-tools python2-numpy  vim zsh git go i3-wm python2 python pacman-contrib expac fzf libpulse 
+sudo pacman -Syy --needed --noconfirm libmpdclient boost-libs perl-data-dump perl-authen-pam perl python-setuptools python2-setuptools sudo fakeroot mtdev gtk2-perl  tslib xorg-xset perl-file-desktopentry perl-gtk3 qt5-translations efibootmgr boost-libs python rsync fuse2 xorg-xauth libxinerama gtk3 libxrandr libxcursor 
+sudo pacman -Syu --needed --noconfirm man-pages imlib2 openssl double-conversion base-devel xmlto docbook-xsl qt5-base xdg-utils inetutils bc pahole patchutils flex python-sphinx python-sphinx_rtd_theme graphviz imagemagick inetutils patchutils libqalculate hicolor-icon-theme gtk-update-icon-cache git sudo fakeroot go binutils rofi  docbook-xsl libdrm patch autoconf automake gcc bdf-unifont help2man ttf-dejavu
 sudo chmod -R 777 .
 sudo mkdir -p /go/pkg/mod/cache/download/github.com
 sudo chmod -R 777 /go
