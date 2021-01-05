2b25e4395dc2351118eb646fed0e798ce89e269b
Ty Ethan Sayahi <tysayahi@gmail.com>
2021-01-05 11:01:13 -0500

    New pkgs, fix error in plymouth pkgbuild
    Former-commit-id: ff4e2a45a254071af0477ee5939c8f499b30c6f1

---
+++ osmium-pkgs/osmium-openbox-skel/src/.config/#rofi/scripts/appsmenu.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rofi -modi run,drun,window -lines 12 -padding 18 -width 60 -location 0 -show drun -sidebar-mode -columns 3 -font "Noto Sans 8"
+
+#rofi -no-lazy-grab -show drun -theme themes/appsmenu.rasi
